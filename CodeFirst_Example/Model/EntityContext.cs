e90c4378e59f13da87e7bfef4da383888d634775
ibrah <ibrah@DESKTOP-6DHQK40>
2021-09-17 17:23:01 +0300

    Add project files.

---
+++ CodeFirst_Example/Model/EntityContext.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirst_Example.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirst_Example.Model
+{
+    public class EntityContext:DbContext
+    {
+        // Kullanılacak tablolar - Tables to Use
+        public DbSet<Products> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Sunucu bağlantı ayarları - SQL Server connection settings
+            optionsBuilder.UseSqlServer(@"Server=[SERVER_NAME] ;Database=[DATABASE_NAME]; Trusted_Connection =True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductMap()); // ProductMap ayarlarını kullan. - Use ProductMap mapping settings.
+        }
+    }
+}
---
+++ CodeFirst_Example/Model/ProductMap.cs
@@ -0,0 +1,20 @@
+﻿using CodeFirst_Example.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CodeFirst_Example.Model
+{
+    public class ProductMap:IEntityTypeConfiguration<Products>
+    {
+        // Mapping Settings
+        public void Configure(EntityTypeBuilder<Products> builder)
+        {
+            builder.HasKey(x => x.Id); //PrimaryKey
+            builder.Property(x => x.Id).ValueGeneratedOnAdd(); // Otomatik Artan - Identity Increment
+            builder.Property(x => x.Name).HasMaxLength(50); // nvarchar(50)
+            builder.Property(x => x.Description).HasMaxLength(150); // nvarchar(150)
+            builder.Property(x => x.Price).HasColumnType("money"); // Convert Decimal to money for SQL table
+            builder.ToTable("Products"); // Tablo Adı - Table Name
+        }
+    }
+}
